9edd6886990092089d80f192d69c165bc20cf0c9
Zhymir Thompson <dzhymir@gmail.com>
2020-10-25 17:53:13 -0400

    converted script to function

--- src/task1/attack2/my_attack.py
+++ src/task1/attack2/my_attack.py
@@ -7,22 +7,27 @@ from utils.file import load_from_json
 from utils.model import load_lenet
 import numpy as np
 
-model_configs = load_from_json('model-config.json')
-data_configs = load_from_json('data-config.json')
-attack_configs = load_from_json('attack-config.json')
 
-# load the targeted model
-model_file = os.path.join(model_configs.get("dir"), model_configs.get("um_file"))
-target = load_lenet(file=model_file, wrap=True)
+def my_attack(model_config, data_config, attack_config):
+    model_configs = load_from_json(model_config)
+    data_configs = load_from_json(data_config)
+    attack_configs = load_from_json(attack_config)
 
-# load the benign samples
-data_file = os.path.join(data_configs.get('dir'), data_configs.get('bs_file'))
-data_bs = np.load(data_file)
-# load the corresponding true labels
-label_file = os.path.join(data_configs.get('dir'), data_configs.get('label_file'))
-labels = np.load(label_file)
+    # load the targeted model
+    model_file = os.path.join(model_configs.get("dir"), model_configs.get("um_file"))
+    target = load_lenet(file=model_file, wrap=True)
 
-# generate adversarial examples for a small subset
-data_bs = data_bs[:100]
-labels = labels[:100]
-generate_ae(model=target, data=data_bs, labels=labels, attack_configs=attack_configs, save=True, output_dir="./results")
+    # load the benign samples
+    data_file = os.path.join(data_configs.get('dir'), data_configs.get('bs_file'))
+    data_bs = np.load(data_file)
+    # load the corresponding true labels
+    label_file = os.path.join(data_configs.get('dir'), data_configs.get('label_file'))
+    labels = np.load(label_file)
+
+    # generate adversarial examples for a small subset
+    data_bs = data_bs[:100]
+    labels = labels[:100]
+    generate_ae(model=target, data=data_bs, labels=labels, attack_configs=attack_configs, save=True, output_dir="./results")
+
+
+my_attack('model-config.json', 'data-config.json', 'atack-config.json')
---
+++ src/task1/general_attack.py
@@ -0,0 +1,3 @@
+from task1.attack2 import my_attack
+
+# call my attack with my_attack(model_config name, data_config name, attack_config name)
